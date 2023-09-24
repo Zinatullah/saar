0a241355421804d0b2a9e8ca437f02fc57d367b3
Zinatullah <zinatullahakrami@gmail.com>
2023-09-24 19:11:18 +0430

    pages corrected

--- backend/pages/add_company.php
+++ backend/pages/add_company.php
@@ -116,7 +116,7 @@
 
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 bg-gray">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                    <h1 class="h2">نوی شرکت اضافه کول</h1>
+                    <h1 class="h2">نوی داخلي شرکت اضافه کول</h1>
                 </div>
                 <div class="my-4 w-100" width="900" height="380">
                     <div class="col-md-7 col-lg-12 col-md-12">
--- backend/pages/company_block.php
+++ backend/pages/company_block.php
@@ -125,7 +125,7 @@ $data = mysqli_fetch_all($result);
 
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                    <h1 class="h2"> بلاک شرکتونه</h1>
+                    <h1 class="h2"> بلاک شوي داخلي شرکتونه</h1>
                 </div>
                 <div class="my-4 w-100" width="900" height="380">
                     <section class="intro" dir='rtl'>
